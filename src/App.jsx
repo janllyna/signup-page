65d2e5894c502fe1b11048dee1b85cae24984b96
Janllyn Avantikha <142758749+janllyna@users.noreply.github.com>
2025-07-22 09:47:55 +0530

    Updated App.jsx with firebase adding functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import './style.css';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, app, db } from './firebase';
+import {setDoc,doc} from "firebase/firestore";
 
 const App = () => {
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPwd, setConfirmPwd] = useState('');
+  const [success, setSuccess] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignUp = async(e) => {
+    try{
+      await createUserWithEmailAndPassword(auth,email,password);
+      const user=auth.currentUser;
+      console.log(user)
+      if(user){
+        await setDoc(doc(db,"Users",user.uid),{
+          email:user.email,
+          firstName:fName,
+          lastName:lName
+        });
+      }
+      console.log("User Registered Successfully");
+    }catch(error){
+      console.log(error.message);
+    }
+    };
   return (
     <div className='Layout'>
       <div className='middleLayout'>
@@ -12,24 +40,24 @@ const App = () => {
             <div className='nameDetails'>
               <form className='firstName'>
               <label htmlFor='fName' id='labelFName'>First Name: </label>
-              <input type="text" id='fName' name='fName' placeholder='First Name'/>
+              <input type="text" id='fName' name='fName' placeholder='First Name' onChange={(e)=>setFName(e.target.value)}/>
             </form>
             <form className='lastName'>
               <label htmlFor='lName'>Last Name: </label>
-              <input type="text" id='lName' name='lName' placeholder='Last Name'/>
+              <input type="text" id='lName' name='lName' placeholder='Last Name' onChange={(e)=>setLName(e.target.value)}/>
             </form>
             </div>
             <form className='emailID'>
               <label htmlFor='ID' id='eID'>Email ID:</label>
-              <input type="text" id='ID' name='ID' placeholder='Email Address'/>
+              <input type="text" id='ID' name='ID' placeholder='Email Address' onChange={(e)=>setEmail(e.target.value)}/>
             </form>
             <form className='Password' id='PwdLabel'>
               <label htmlFor='Pwd'>Password: </label>
-              <input type='password' id='Pwd' name='Pwd' placeholder='Password' />
+              <input type='password' id='Pwd' name='Pwd' placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
             </form>
             <form className='confirmPassword'>
               <label htmlFor='confirmPwd'id=''>Confirm Password:</label>
-              <input type='password' id='confirmPwd' name='confirmPwd' placeholder='Confirm Password' />
+              <input type='password' id='confirmPwd' name='confirmPwd' placeholder='Confirm Password' onChange={(e)=>setConfirmPwd(e.target.value)}/>
             </form>
           </div>
           <div className='otherOptions'>
@@ -47,7 +75,7 @@ const App = () => {
           </div>
           <div className='actionButtons'>
             <div className='signUpButton'>
-              <button className='signUp'>
+              <button className='signUp' onClick={handleSignUp}>
                 Sign Up
               </button>
             </div>
@@ -57,6 +85,9 @@ const App = () => {
               </button>
             </div>
           </div>
+        </div>
+        <div className='loginImg'>
+            <img src={require('./loginimg.jpg')} />
         </div>
       </div>
     </div>
